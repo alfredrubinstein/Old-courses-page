552051318f3bd2163b8904fbad80a4a5d9288130
alfredrubinstein <rubinsteinalfred@gmail.com>
2024-07-04 11:42:58 +0300

    pequeno arreglo en el readme

--- README.js
+++ README.js
@@ -1,7 +1,8 @@
 #to run the page:
 #1- open the terminal in the "COURSES PAGE" folder
-#2- type "npm run moshico"
-#3- then open the browser and type "http://localhost:3000/" in the search bar
+#2- enter in the console "npm i"
+#3- type "npm run moshico"
+#4- then open the browser and type "http://localhost:3000/" in the search bar
 
 
 
